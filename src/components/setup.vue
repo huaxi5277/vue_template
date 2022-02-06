36c1c6f5b1d447cd7624f0118681533ca3c0425f
huaxi <ahj52777@163.com>
2022-02-06 19:15:11 +0800

    模版提交

---
+++ src/components/setup.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>HelloWorld</h1>
+    <ul>
+      <li v-for="(item, index) in user" :key="index" @click="changeUser(index, $event)">
+        <p>{{item.name}}</p>
+        <p>{{item.age}}</p>
+        <p>{{item.sex}}</p>
+        
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+export default defineComponent({
+  name: 'Setup',
+  props: {
+    user: Object
+  },
+  setup(props, ctx) {
+    function changeUser(index) {
+      ctx.emit('change', index);
+    }
+    return {changeUser}
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{name}}
+    {{student.name}}
+    <button @click="changeName">测试监听</button>
+    <Setup :user="user" @change="changeUser"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
+import Setup from '@/components/setup.vue'
+import {ref, defineComponent, reactive, watch} from 'vue'
+export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld
+    Setup
+  },
+  setup() {
+    const name = ref('huaxi');
+    const student = reactive({
+      name: 'huaxi',
+      age: 13
+    })
+    const user = reactive([
+      {
+      name: 'huaxi',
+      age: 13,
+      sex: '男'
+    },
+    {
+      name: 'xiaobo',
+      age: 16,
+      sex: '女'
+    },
+    ])
+    watch([name, () => student.name], (newValue, oldValue)=> {
+      console.log(newValue)
+      console.log(oldValue)
+    })
+    function changeUser(index) {
+      user[index].name = 'gouzi';
+    }
+    function changeName() {
+      student.name = '111'
+    }
+    return {name, user, student, changeName, changeUser}
   }
-}
+})
 </script>
